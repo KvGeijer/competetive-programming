0c62a12d468fc010a376018ad1b4c93909b49fc7
Kåre von Geijer <kare.kvg@gmail.com>
2023-10-07 20:54:24 +0200

    Add simple C++ template

---
+++ templ.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define sz(x) (int)(x).size()
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define nl "\n"
+#define nf endl
+#define ll long long
+#define uint unsigned int
+#define pb push_back
+#define _ << " " <<
+#define mod 1'000'000'007
+#define mp make_pair
+#define trav(a, x) for (auto& a : x)
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef multiset<ll> mll;
+typedef pair<double, double> pdd;
+typedef array<ll, 4> tupl;
+ll INF = 1'000'000'007;
+
+ll n, t, s, m, sum, d;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
